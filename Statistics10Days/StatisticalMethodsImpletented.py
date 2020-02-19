3cd4daa0f12d84460f6090b559b63ec327cc5534
TarunVallabhuni <cunningjack@gmail.com>
2020-02-19 16:21:37 +0100

    Create StatisticalMethodsImpletented.py

---
+++ Statistics10Days/StatisticalMethodsImpletented.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+"""
+Implementations of different Statistical methods in pure Python.
+"""
+
+def mean(data):
+    """Return the sample arithmetic mean of data."""
+    n = len(data)
+    if n < 1:
+        raise ValueError('mean requires at least one data point')
+    return sum(data)/n # in Python 2 use sum(data)/float(n)
+
+def _ss(data):
+    """Return sum of square deviations of sequence data."""
+    c = mean(data)
+    ss = sum((x-c)**2 for x in data)
+    return ss
+
+def stddev(data, ddof=0):
+    """Calculates the population standard deviation
+    by default; specify ddof=1 to compute the sample
+    standard deviation."""
+    n = len(data)
+    if n < 2:
+        raise ValueError('variance requires at least two data points')
+    ss = _ss(data)
+    pvar = ss/(n-ddof)
+    return pvar**0.5
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+    
+    fptr.write( str(mean(arr))+'\n')
+    fptr.write( str(stddev(arr))+'\n')
+    
+    fptr.close()
+  
+# Sample Input  
+# 10
+# 64630 11735 14216 99233 14470 4978 73429 38120 51135 67060
+
+# Sample Output
+
