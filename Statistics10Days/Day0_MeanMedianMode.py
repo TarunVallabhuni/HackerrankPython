fe31c8271edf3942753778de7f465f2e5bb69897
TarunVallabhuni <cunningjack@gmail.com>
2020-02-19 15:26:43 +0100

    Create Day0_MeanMedianMode.py

---
+++ Statistics10Days/Day0_MeanMedianMode.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import statistics
+import pandas as pd 
+
+# Complete the reverseArray function below.
+def summaryStatistics(a):
+    mean = statistics.mean(a)
+    median = statistics.median(a)
+    df = pd.DataFrame(a)
+    mode = df.mode()[0][0]
+
+    fptr.write( str(mean)+'\n')
+    fptr.write( str(median)+'\n')
+    fptr.write( str(mode)+'\n')
+    fptr.write('\n')
+    return 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    summaryStatistics(arr)
+
+
+
+    fptr.close()
